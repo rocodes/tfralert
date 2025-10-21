6cc29c0e2381e9fc88a5616b336fe5be93972fbe
Rowen S <ro@freedom.press>
2025-10-21 09:57:08 -0400

    license header on main and logic .rs

--- src/logic.rs
+++ src/logic.rs
@@ -1,3 +1,11 @@
+// Copyright 2025. This file is part of TFRAlert.
+
+// TFRAlert is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+// TFRAlert is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with TFRAlert. If not, see <https://www.gnu.org/licenses/>.
+
 use anyhow::Result;
 use log::{debug, error, info};
 use reqwest::Client;
