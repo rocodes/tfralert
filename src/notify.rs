8616dd728353c53eb119066135c231694dd9ee85
Rowen S <ro@freedom.press>
2025-10-21 09:56:45 -0400

    fixup notif text

--- src/notify.rs
+++ src/notify.rs
@@ -1,3 +1,11 @@
+// Copyright 2025. This file is part of TFRAlert.
+
+// TFRAlert is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+// TFRAlert is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with TFRAlert. If not, see <https://www.gnu.org/licenses/>.
+
 use crate::logic::ParsedTFREvent;
 use notify_rust::Notification;
 
@@ -57,7 +65,7 @@ fn build_batch_notification(events: &[ParsedTFREvent]) -> Option<NotificationTex
     }
 
     Some(NotificationText {
-        title: format!("{} new TFRs detected", events.len()),
+        title: format!("{} TFRs", events.len()),
         body,
     })
 }
